030f9a34c7eb511530d3ea1b79d0d74e63dbb5ae
shubhusion <shubham27.sharma03@gmail.com>
2023-09-03 14:10:58 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<shubhusion"
+export GITHUB_USERNAME="shubhusion"
 
 # Export the GitHub repository name
 export GITHUB_REPOSITORY="se2001-21f2000041"
